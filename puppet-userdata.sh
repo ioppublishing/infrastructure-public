4727d6513cac9e716c61dbb3060e4c7226714c27
Matthew Brown <matthew.brown@ioppublishing.org>
2021-05-05 11:21:35 +0100

    Hacky change to get the crl.

--- puppet-userdata.sh
+++ puppet-userdata.sh
@@ -225,14 +225,15 @@ function associatenode {
     #clear out extraneous certs and generate a new one
     ${PUPPET} bootstrap purge
     ${PUPPET} bootstrap csr
-    establishtrust
     # submit the cert
     ASSOCIATE_TOKEN=$(aws opsworks-cm associate-node --region ${ocm_region} --server-name ${ocm_server} --node-name ${CERTNAME} --engine-attributes Name=PUPPET_NODE_CSR,Value="`cat $PP_CSR_PATH`" --query "NodeAssociationStatusToken" --output text)
     #wait
     aws opsworks-cm wait node-associated --region ${ocm_region} --node-association-status-token "${ASSOCIATE_TOKEN}" --server-name ${ocm_server}
     #install and verify
     aws opsworks-cm-puppet describe-node-association-status --region ${ocm_region} --node-association-status-token "${ASSOCIATE_TOKEN}" --server-name ${ocm_server} --query 'EngineAttributes[0].Value' --output text > ${PP_CERT_PATH}
-    puppet agent -t || true
+    establishtrust
+    # ridiculous change to get the CRL
+    puppet agent -t --noop|| true
     ${PUPPET} bootstrap verify
 }
 
